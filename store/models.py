70e8e650fa8325d26ef201c65fff554c274d5ba3
sai1512 <sainaidu1512@gmail.com>
2026-02-18 20:49:28 +0100

    Creating migrations

--- store/models.py
+++ store/models.py
@@ -11,8 +11,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255) 
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -58,6 +59,7 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Address(models.Model):
+    zip = models.CharField(max_length=10)
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
