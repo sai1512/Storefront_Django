06f83ddb64e0f01ed1948d177812807ef5cec880
sai1512 <sainaidu1512@gmail.com>
2026-02-19 14:59:22 +0100

    new migration with rename firstname

--- store/models.py
+++ store/models.py
@@ -30,7 +30,7 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'SILVER'),
         (MEMBERSHIP_GOLD, 'GOLD'),
     ]
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=12)
